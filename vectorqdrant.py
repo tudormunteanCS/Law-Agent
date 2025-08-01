fc96c95a747d1d5628ed6b6ac20d90dd22bfaeb2
Muntean Tudor <tudormunt@gmail.com>
2025-08-01 16:29:13 +0300

    script prepared for upserting points in qdrant (embeddings)

--- vectorqdrant.py
+++ vectorqdrant.py
@@ -9,13 +9,6 @@ from openai import OpenAI
 import uuid
 
 
-# qdrant_client = QdrantClient(
-#     url="https://14c4547c-e9c4-4793-ab4a-3834d017892c.europe-west3-0.gcp.cloud.qdrant.io:6333",
-#     api_key=qdrant_api_key,
-#     check_compatibility=False
-# )
-# print(qdrant_client.get_collections())
-
 def create_collection(client, collection_name, DIMENSIONS):
     if not client.collection_exists(collection_name):
         print(f"Collection {collection_name} does not exist. Creating it...")
@@ -49,17 +42,16 @@ def embedd_texts(texts, openai_api_key, dimensions):
         input=texts,
         dimensions=dimensions,
         encoding_format="float"
-    )
-    return [embedding['embedding'] for embedding in resp['data']]  # Extract embeddings from response
+    ).data[0].embedding
+    return resp
 
 
 if __name__ == "__main__":
-    openai_api_key = os.getenv("OPENAI_API_KEY")
+    openai_api_key = os.getenv("OPENAI_Lawgentic_API_KEY")
     qdrant_api_key = os.getenv('QDRANT_API_KEY')
     data = Path("processed_data")
     collection_name = "legi_ro"
     DIMENSIONS = 1536
-    BATCH_SIZE = 300
 
     client = QdrantClient(
         url="https://14c4547c-e9c4-4793-ab4a-3834d017892c.europe-west3-0.gcp.cloud.qdrant.io:6333",
@@ -70,28 +62,31 @@ if __name__ == "__main__":
     # creating the collection only ran once
     create_collection(client, collection_name, DIMENSIONS)
     all_files = list(data.glob("*.json"))
+
+    ids = []
+    payloads = []
+    vectors = []
     for file_path in tqdm(all_files):
         with open(file_path, "r", encoding="utf-8") as f:
             json_data = json.load(f)
-        for batch in chunked(json_data, BATCH_SIZE):
-            texts = [chunk["text"] for chunk in batch]
-            vectors = embedd_texts(texts, openai_api_key, DIMENSIONS)
-            payloads = [
+        for chunk in json_data:
+            text = chunk["text"]
+            vectors.append(embedd_texts(text, openai_api_key, DIMENSIONS))
+            payloads.append(
                 {
                     "sursa": chunk["sursa"],
                     "articol": chunk["articol"],
                     "aliniat": chunk["alin"],
                     "text": chunk["text"],
-                } for chunk in batch
-            ]
-            # construct list of ids
-            ids = [uuid.uuid4() for chunk in batch]
-
-            client.upsert(
-                collection_name=collection_name,
-                points=qdrant.Batch(
-                    ids=ids,
-                    vectors=vectors,
-                    payloads=payloads
-                )
+                }
             )
+            ids.append(str(uuid.uuid4()))
+
+    client.upsert(
+        collection_name=collection_name,
+        points=qdrant.Batch(
+            ids=ids,
+            vectors=vectors,
+            payloads=payloads
+        )
+    )
